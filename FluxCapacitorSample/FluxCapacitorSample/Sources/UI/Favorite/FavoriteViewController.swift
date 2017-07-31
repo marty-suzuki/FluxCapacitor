20cbfe93613bf8f3cffbb4559113d316e25ea683
marty-suzuki <s1180183@gmail.com>
2017-08-01 03:17:29 +0900

    add ViewControllers

---
+++ FluxCapacitorSample/FluxCapacitorSample/Sources/UI/Favorite/FavoriteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteViewController.swift
+//  FluxCapacitorSample
+//
+//  Created by marty-suzuki on 2017/08/01.
+//  Copyright © 2017年 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ FluxCapacitorSample/FluxCapacitorSample/Sources/UI/Search/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  FluxCapacitorSample
+//
+//  Created by marty-suzuki on 2017/08/01.
+//  Copyright © 2017年 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
