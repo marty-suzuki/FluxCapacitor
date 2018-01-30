b89cb366809dcf317f85a29bca719309124d1cee
marty-suzuki <s1180183@gmail.com>
2018-01-30 23:53:19 +0900

    fix PThreadMutex

--- FluxCapacitor/PThreadMutex.swift
+++ FluxCapacitor/PThreadMutex.swift
@@ -9,21 +9,23 @@
 import Foundation
 
 final class PThreadMutex {
-    private var mutex: pthread_mutex_t = pthread_mutex_t()
+    private let mutex = UnsafeMutablePointer<pthread_mutex_t>.allocate(capacity: 1)
     
     init() {
-        pthread_mutex_init(&mutex, nil)
+        pthread_mutex_init(mutex, nil)
     }
     
     deinit {
-        pthread_mutex_destroy(&mutex)
+        pthread_mutex_destroy(mutex)
+        mutex.deinitialize()
+        mutex.deallocate(capacity: 1)
     }
     
     func lock() {
-        pthread_mutex_lock(&mutex)
+        pthread_mutex_lock(mutex)
     }
     
     func unlock() {
-        pthread_mutex_unlock(&mutex)
+        pthread_mutex_unlock(mutex)
     }
 }
